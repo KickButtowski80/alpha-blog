29dd571c7be81dda58aa0adec0c865b7703519dd
kickbuttowski59 <pazpaz22@yahoo.com>
2018-08-22 00:46:41 +0000

    added before_action inside article controller to avoid redendency

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    
+    before_action :set_article, only: [:show, :edit, :update, :destroy] 
     def index
         @articles = Article.all
     end
@@ -20,15 +20,15 @@ class ArticlesController < ApplicationController
     end
     
    def show
-       @article = Article.find(params[:id])
+      
    end
     
   def edit
-      @article = Article.find(params[:id])
+     
   end
   
   def update
-      @article = Article.find(params[:id])
+     
       if @article.update(article_params)
           flash[:notice] = "an Article was successfully edited" 
           redirect_to article_path(@article)
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])  
+     
     if @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -48,6 +48,10 @@ class ArticlesController < ApplicationController
     private 
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+        
+        def set_article
+            @article = Article.find(params[:id])
         end
  
 end
