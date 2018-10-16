d8d6b322f61b8fdc1fb2ae7808e01dd4332398f7
kickbuttowski59 <pazpaz22@yahoo.com>
2018-10-16 17:51:05 +0000

    installed faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+5.times do    
+    
+    Article.create!(title: Faker::Artist.name, description: Faker::Lorem.paragraph, user_id: User.last.id) 
+    
+end
