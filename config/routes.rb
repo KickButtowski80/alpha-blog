aafe74f13a02c0a213dc056851fe4cafceff4a07
kickbuttowski59 <pazpaz22@yahoo.com>
2018-10-16 16:34:55 +0000

    Added edit and update actions and DRY for edit and update UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'pages#homepage'
   get 'about' => 'pages#about'
   
   resources :articles
+  
+  get 'signup' , to: 'users#new'
+  # post 'users', to: 'users#create'
+  # or 
+  resources :users, except: [:new]
 end
