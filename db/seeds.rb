700160e5f3cf8a6955995f2cfcdb3cc30c6408c4
kickbuttowski59 <pazpaz22@yahoo.com>
2018-10-17 18:50:36 +0000

    Installed will_paginate and bootstrap-will_paginate gems and added the functionality to article index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do    
-    
+10.times do     
     Article.create!(title: Faker::Artist.name, description: Faker::Lorem.paragraph, user_id: User.last.id) 
     
 end
