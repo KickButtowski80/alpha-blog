8523afde9dd91f8775969fbf4b164c3d38772370
kickbuttowski59 <pazpaz22@yahoo.com>
2018-10-19 01:38:13 +0000

    got rid of debugger statement in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      debugger
           session[:user_id] = @user.id
           flash[:success] = "Welcome to alpha blog #{@user.username}"
           redirect_to user_path(@user)
