ddddf9117cdce3df0f991f061209e36f864ec7dc
kickbuttowski59 <pazpaz22@yahoo.com>
2018-10-31 02:18:53 +0000

    made test case for a category show page

--- test/integration/show_a_category_test.rb
+++ test/integration/show_a_category_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class ShowACategoryTest < ActionDispatch::IntegrationTest
     
     def setup
-        @category = Category.create! name: "Red"
-        
+        @category = Category.create! name: "Red"        
     end
     
     
     test 'show a category page' do 
         get category_path @category
-        assert_match "Red", response.body            
+        # assert_match "Red", response.body       
+        assert_select "h1",  text: @category.name
     end
     
     
